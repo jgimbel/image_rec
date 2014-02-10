728f5415c458747d6511e82dfa66e1d5c0d77188
joel <joel@Elizabeth.(none)>
2014-02-10 13:18:49 -0600

    removing unused files(hopefully) - Joel

---
+++ image_rec/src/image_publish.cpp
@@ -0,0 +1,26 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv/cvwimage.h>
+#include <opencv/highgui.h>
+#include <cv_bridge/cv_bridge.h>
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "image_publisher");
+  ros::NodeHandle nh;
+  image_transport::ImageTransport it(nh);
+  image_transport::Publisher pub = it.advertise("camera/image", 1);
+
+  cv::WImageBuffer3_b image( cvLoadImage("/dev/video0", CV_LOAD_IMAGE_COLOR) );
+  cv::Mat imageMat(image.Ipl());
+
+  cv_bridge::CvImage out_msg;
+  out_msg.image = imageMat;
+
+  ros::Rate loop_rate(5);
+  while (nh.ok()) {
+    pub.publish(out_msg.toImageMsg());
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+}
